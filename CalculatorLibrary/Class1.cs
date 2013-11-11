ed6100a3fd2636507a9bad03b5fae621d1971251
ragha <raghadeepthi1@gmail.com>
2013-11-11 05:21:40 -0800

    Commited the changes

---
+++ CalculatorLibrary/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public delegate int MathOperation(int a ,int b);
+    public class Calculator
+    {
+        private MathOperation Operation;
+        //Constructor...
+        public Calculator(MathOperation op)
+        {
+            Operation = op;
+        }
+        //Method.. `                                                                                                                                                        
+        public void CalculateAndPrint(int a, int b)
+        {
+            int res = Operation(a, b);
+            Console.WriteLine(res);
+
+        }
+
+    }
+}
---
+++ ClientApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CalculatorLibrary;
+
+class MyMath
+{
+    public int add(int a, int b)
+    {
+        return (a + b);
+    }
+}
+
+class DelegateDemo
+{
+    static void Main()
+    {
+        MyMath m = new MyMath();
+        MathOperation mo = new MathOperation(m.add);
+        Calculator cal = new Calculator(mo);
+        cal.CalculateAndPrint(10, 30);
+
+    }
+}
+
